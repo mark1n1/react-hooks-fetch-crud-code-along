a652faac63e315f0f6335d533658ae69efd2e24c
Marcos Valderrabano <ing.marco.valu@gmail.com>
2022-06-24 14:26:30 -0700

    Completed lecture

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,15 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
+
+const URL = 'http://localhost:4000/items';
 
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
 
-  function handleCategoryChange(category) {
-    setSelectedCategory(category);
-  }
+  useEffect(() => {
+    fetch(URL)
+      .then((r) => r.json())
+      .then((items) => setItems(items));
+  }, []);
 
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
@@ -17,16 +21,46 @@ function ShoppingList() {
     return item.category === selectedCategory;
   });
 
+  function handleCategoryChange(category) {
+    setSelectedCategory(category);
+  }
+
+  function handleAddItem(newItem) {
+    setItems([...items, newItem]);
+  }
+
+  function handleUpdateItem(updateItem) {
+    const updatedItems = items.map((item) => {
+      if(item.id === updateItem.id) {
+        return updateItem;
+      } else {
+        return item;
+      }
+    });
+    
+    setItems(updatedItems);
+  }
+
+  function handleDeleteItem(deletedItem) {
+    const updatedItems = items.filter((item) => item.id !== deletedItem.id);
+    setItems(updatedItems);
+  }
+
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onAddItem={ handleAddItem }/>
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item 
+            key={item.id} 
+            item={item} 
+            onUpdateItem={handleUpdateItem} 
+            onDeleteItem={handleDeleteItem}
+          />
         ))}
       </ul>
     </div>
